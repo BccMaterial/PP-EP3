621b09910e798d3f5585b596d7ba0c4926a93f0e
thiagopls1 <thiagopls1@hotmail.com>
2025-11-08 15:02:06 -0300

    style: add colors

--- src/client/main.go
+++ src/client/main.go
@@ -1,34 +1,45 @@
 package main
 
 import (
-  "net"
-  "os"
-  "io"
-  "log"
-  "fmt"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Magenta = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
+
 func mustCopy(dst io.Writer, src io.Reader) {
-  if _,err := io.Copy(dst, src); err != nil {
-    log.Fatal(err)
-  }
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-  conn, err := net.Dial("tcp", "localhost:3000")
-  fmt.Println("Connected!")
-  if err != nil {
-    log.Fatal(err)
-  }
+	conn, err := net.Dial("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  done:= make(chan struct{})
-  
-  go func() {
-    io.Copy(os.Stdout, conn)
-    log.Println("done")
-    done <- struct{}{} // sinaliza para a gorrotina principal
-  }()
-  mustCopy(conn, os.Stdin)
-  conn.Close()
-  <-done // espera a gorrotina terminar
+	fmt.Println(Green + "Conectado com sucesso! Bem-vindo!" + Reset)
+
+	done := make(chan struct{})
+
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println(Yellow + "[Servidor]: Fechando o chat" + Reset)
+		done <- struct{}{} // sinaliza para a gorrotina principal
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // espera a gorrotina terminar
 }
