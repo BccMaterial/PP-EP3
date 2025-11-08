945c5e81e814e5e52473f538bab11a13210b487c
thiagopls1 <thiagopls1@hotmail.com>
2025-11-08 15:45:10 -0300

    feat: add prompt message

--- src/client/main.go
+++ src/client/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -18,13 +18,8 @@ var Cyan = "\033[36m"
 var Gray = "\033[37m"
 var White = "\033[97m"
 
-func mustCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
+	prompt := "> "
 	conn, err := net.Dial("tcp", "localhost:3000")
 	if err != nil {
 		log.Fatal(err)
@@ -33,13 +28,42 @@ func main() {
 	fmt.Println(Green + "Conectado com sucesso! Bem-vindo!" + Reset)
 
 	done := make(chan struct{})
+	messages := make(chan string)
 
+	// Recebe as mensagens
 	go func() {
-		io.Copy(os.Stdout, conn)
-		log.Println(Yellow + "[Servidor]: Fechando o chat" + Reset)
-		done <- struct{}{} // sinaliza para a gorrotina principal
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			messages <- scanner.Text()
+		}
+		close(messages)
 	}()
-	mustCopy(conn, os.Stdin)
+
+	// Printa as mensagens
+	go func() {
+		for message := range messages {
+			// Limpamos o prompt e repetimos o digite a mensagem. Dessa forma,
+			// o prompt continua aparecendo mesmo recebendo uma mensagem.
+			fmt.Print("\r\033[K")
+			fmt.Println(message)
+			fmt.Print(prompt)
+		}
+		done <- struct{}{}
+	}()
+
+	// Loop principal para enviar mensagens
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		fmt.Print(prompt)
+		message := scanner.Text()
+		if message == "" {
+			continue
+		}
+
+		fmt.Fprintln(conn, message)
+	}
+
 	conn.Close()
-	<-done // espera a gorrotina terminar
+	<-done
 }
